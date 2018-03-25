96ae8c87550cc42b92f0f635c691e1ece4a9657a
YBingo <Y_Bingo@163.com>
2018-03-26 00:04:54 +0800

    重新定义了一下后台数据的数据结构

--- server/model.js
+++ server/model.js
@@ -4,6 +4,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/together");
 
 const model = {
     user : {
+        uid: {"type":Number,require:true} ,//  这是一个用户ID
         user_name : {"type":String, require:true},//用户名
         user_pwd : {"type":String, require:true},//用户密码
         user_creat_time : {"type":Date, default:Date.now}, // 创建时间
@@ -11,42 +12,96 @@ const model = {
         user_sex : {type:String, enum:["boy","girl"]},//性别
         user_city : {type:String, default:"广东工业大学"},//所在城市
         user_head : {type :String, default:'boy'}, // 头像
-        user_touch : {type:String, default:"你没有留下任何联系方式"},//用户的联系方式
+        user_touch : {//用户的联系方式
+            QQ : {type:String, default: ""},
+            tel: {type:Number, default: 110}
+        },
         user_signatrue : {type:String, default:"这个用户很懒，什么都没留下"},//用户签名
-        user_love_topic : {type:Array, default:[]},//用户喜爱的活动类型
+        user_love_topic : [//用户喜爱的活动类型
+            {
+                topic_type : String // 类型
+            }
+        ]
         
     },
     topic :{
+        tid : {"type":Number,require: true}, // 这是一个主题id
         topic_title : {type:String,require:true},//活动标题
         topic_dec : {type:String,require:true},//活动内容,
         topic_type : {topic:Array,default:["ALL"]},//活动类型，判断这是哪种类型的活动
-        topic_from : {type:String,require:true},//活动发起人，
-        topic_from_touch : {type:String},//活动发起人的联系方式
+        topic_from : {//活动发起人，
+            uid:        {type:String, require:true},// 用户ID
+            user_name : {type: String ,require:true}, // 用户名 
+            user_head : {type : String ,require:true},// 用户头像
+            user_touch: { type: String, default: "你没有留下任何联系方式" },//用户的联系方式
+        },
+        topic_face: {type: String ,default:["All"]}, // 活动主要面向人群
         topic_photo : {type:Array} ,//活动的活动图片,
         topic_create_time : {type:Date,default:Date.now},//活动的开始时间
         topic_begin_time : {type:Date,default:Date.now},//活动的开始时间
         topic_during_time : {type:String,default:"1h"},//活动持续时间
         topic_end_time : {type:Date,require:true},//活动报名的截止时间
         topic_place : {type:String,default:null},//活动的地点，
-        topic_menber : {type:Number,default:null},//该活动的预计参加人数
+        topic_num : {type: Number,defalult : 0},// 活动预计参加人数
+        topic_menber : [ // 活动参加成员
+            {
+                uid: Number, // 用户ID
+                user_name: String, // 用户名
+                user_head: String, // 用户头像
+            }
+        ],
         topic_had_join : {type:Number,default:0},//活动已经参与的人数
-        topic_money : {type:String,defalut:"0元"},//活动预算
-
-        topic_love : {type:Number,default:0},//被点赞数
-        topic_collected : {type:Number,default:0},//被收藏数
+        topic_money :    {type:String,defalut:"0元"},//活动预算
+        topic_love :     {type:Number,default:0},//被点赞数
+        topic_collected :{type:Number,default:0},//被收藏数
         topic_comments : {type:Number,default:0},//被评论数
     },
+    tap :{ // 活动类型
+        tap_id : Number , // 类型ID
+        tap_type: String  // 类型名
+    },
     comment : {
-        com_topic : {type:String,require:true},//评论在哪个活动
-        com_dec : {type:String,require:true},//评论的主要内容
+        com_topic: {// 是在哪个活动中评论的
+            tid: Number , // 活动ID
+            topic_title: String // 活动标题
+        },
+        com_dec :  {type:String,require:true},//评论的主要内容
         com_from : {type:String,require:true},//评论来之谁
-        com_to : {type:String,default:""} ,//评论给谁
+        com_to :   {type:String,default:""} ,//评论给谁
         com_time : {type:Date,default:Date.now},//评论时间
     },
-    user_care : {
-        care_from : String, //关注发起人
-        care_to : String,//被关注的人
+    care : {  // 用户关注
+        care_id : {type: Number, require:true}, // 关注ID
+        care_from : {
+            uid : Number,// 用户ID
+            user_name: String,// 用户名
+        }, //关注发起人
+        care_to: { //被关注的人
+            uid : Number,
+            user_name : String ,//用户名
+        }
     },
+    collect : { // 收藏 
+        collect_id : {type:Number, require:true}, // ID 
+        user : { //
+            uid : String , // 收藏用户的ID
+            user_name: String //收藏用户的名字
+        },
+        topic :{
+            tid: {type: Number, require:true},  // 被收藏的主题ID
+            topic_title: String , // 收藏的标题
+        }
+    },
+    recommend :{ // 推荐给用户的主题
+        rid: {type: Number, require: true },// 推荐ID
+        uid: { type: Number, require: true },// 用户ID
+        topics: [
+            {
+                tid: { "type": Number, require: true }, // 这是一个主题id
+                topic_title: { type: String, require: true }//活动标题
+            }
+        ]
+    }
 }
 
 //批量注册模型
--- src/reducer/commetn.reducer.js
+++ src/reducer/commetn.reducer.js
@@ -1,32 +1,25 @@
 // action
 import * as Action from '../action/comment.action';
+// com_topic: { type: String, require: true },//评论在哪个活动
+// com_dec: { type: String, require: true },//评论的主要内容
+// com_from: { type: String, require: true },//评论来之谁
+// com_to: { type: String,default: "" } ,//评论给谁
+// com_time: { type: Date,default: Date.now },//评论时间
 const initState = {
-    isLoad:true , // 是否重新渲染页面
-    loadMore: true , // 是否后去更多的数据了
-    pageIndex : 0 , // 当前浏览到第几页
-    topic_data : [
+    commentList:[
         {
-            is_good : false,
-            is_collect:true,
-            is_join:false,
-            is_care:false,
-            good_nums: 1,
-            // collect_nums : 33,
-            comment_nums: 1,
-            menber_nums: 1,
-            tid : 1
-        },
-        {
-            is_good: true,
-            is_collect: false,
-            is_join: true,
-            good_nums: 2,
-            // collect_nums : 33,
-            comment_nums: 2,
-            menber_nums: 2,
-            tid :2
+            com_topic: '001',
+            com_dec: "这是第一条评论 ",
+            com_from : {
+                user_name: 'yb',
+                user_head: "boy",
+                
+            },
+            com_to: "002",
+            com_time: new Date().getSeconds(),
+            tid: "t001",
         }
-    ]
+    ],
 };
 
 
@@ -38,10 +31,11 @@ export default function topic(state = initState, action) {
         case Action.DELETE:
             return {...state,...orthers};
         case Action.REPLY : {
-            return {...stage,...orthers};
+            return {...state,...orthers};
         }
         case Action.WATCH : {
-
+            console.log("观看评论");
+            return {state, ...orthers}
         }
         default:
             return state;
